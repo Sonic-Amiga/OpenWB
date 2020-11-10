68b158c1839839e926546beaae952ed5a2ff6784
Pavel Fedin <pavel_fedin@mail.ru>
2020-11-11 01:41:14 +0300

    Change reload value for TIM3 to 65535
    Timer appears to get stuck when RR is set to zero. Looks like it never
    underflows and never sets event.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -170,7 +170,7 @@ static void MX_TIM3_Init(void)
   /* USER CODE END TIM3_Init 1 */
   TIM_InitStruct.Prescaler = 8;
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_DOWN;
-  TIM_InitStruct.Autoreload = 0;
+  TIM_InitStruct.Autoreload = 65535;
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
   LL_TIM_Init(TIM3, &TIM_InitStruct);
   LL_TIM_DisableARRPreload(TIM3);
